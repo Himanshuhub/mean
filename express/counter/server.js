fbc8be4e173e193893c150ea17c041cb248acef4
Himanshu <himanshuguptams@gmail.com>
2017-07-07 19:23:24 -0700

    12

--- express/counter/server.js
+++ express/counter/server.js
@@ -1,8 +1,14 @@
 // Load the express module (Where do you think this comes from?)
 var express = require("express");
 var session = require('express-session');
-// invoke var express and store the resulting application in var app
 var app = express();
+
+// require body-parser
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({extended: true}));
+
+// invoke var express and store the resulting application in var app
 app.set('view engine', 'ejs');
 app.use(session({secret: 'codingdojorocks'}));  // string for encryption
 
@@ -17,6 +23,16 @@ app.get('/', function(request, response) {
   response.render('index', {users: request.session.name});
 })
 // notice that the function is app.get(...) why do you think the function is called get?
+
+app.post('/addtwo', function(request, response) {
+  request.session.name += 1;
+  response.redirect('/');
+})
+
+app.post('/reset', function(request, response) {
+  request.session.name = 0;
+  response.redirect('/');
+})
 
 // this is the line that tells our server to use the "/static" folder for static content
 app.use(express.static(__dirname + "/static"));
