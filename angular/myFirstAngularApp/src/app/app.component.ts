e5e425fe80c623282a8bb8e9d6d1647202eb4ce1
Himanshu <himanshuguptams@gmail.com>
2017-07-18 13:39:14 -0700

    28

--- angular/myFirstAngularApp/src/app/app.component.ts
+++ angular/myFirstAngularApp/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color='red';
+  myBoolean = true;
+  myArray = [1,2,3,4,5];
+  number = 10;
+  name = 'Bill Gates';
+  user = {
+    firstName: 'Darth',
+    lastName: 'Vader'
+  }
+	amount = 1.20;
+	today = new Date();  
   title = 'app';
 }
