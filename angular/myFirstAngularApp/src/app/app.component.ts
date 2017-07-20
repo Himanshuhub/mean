9ffd1d878d42688d979c955f9594e9a8be164259
Himanshu <himanshuguptams@gmail.com>
2017-07-20 08:07:21 -0700

    29

--- angular/myFirstAngularApp/src/app/app.component.ts
+++ angular/myFirstAngularApp/src/app/app.component.ts
@@ -6,15 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user = {
+    firstName: '',
+    lastName: ''
+  }
+  onSubmit(){
+    console.log("onSubmit()");
+    console.log("Hi I'm printing on submit button");
+  };
+
+
+
+  num: number = 1;
+  logNum(){
+    this.num ++;
+    console.log('num is: ', this.num);    
+  };
+  
   color='red';
   myBoolean = true;
   myArray = [1,2,3,4,5];
   number = 10;
   name = 'Bill Gates';
-  user = {
-    firstName: 'Darth',
-    lastName: 'Vader'
-  }
+  // user = {
+  //   firstName: 'Darth',
+  //   lastName: 'Vader'
+  // }
 	amount = 1.20;
 	today = new Date();  
   title = 'app';
